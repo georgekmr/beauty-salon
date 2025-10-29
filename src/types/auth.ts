962008dc2a11d3589dc9cb1d8a667b896dc85d48
George Karl Real <130721675+georgekarlr@users.noreply.github.com>
2025-10-30 00:08:59 +0800

    Updated auth.ts

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -12,10 +12,9 @@ export interface Session {
   user: User
 }
 
-export type PersonaType = 'admin' | 'staff'
 
 export interface PersonaData {
-  type: PersonaType
+  type: string
   email: string
   id?: number // Database ID from validation
   loginName?: string // Only for staff - login username
